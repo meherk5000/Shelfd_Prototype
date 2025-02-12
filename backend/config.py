b5ef0f46825fdd54f00409ff4399e9558e7784b1
Meher Khan <meherk5000@gmail.com>
2025-02-12 21:58:11 +0000

    :)2

--- backend/config.py
+++ backend/config.py
@@ -4,24 +4,24 @@ from urllib.parse import quote_plus
 from fastapi.middleware.cors import CORSMiddleware
 
 class Settings(BaseSettings):
-    jwt_secret_key: str
-    jwt_algorithm: str
-    access_token_expire_days: int
-    mongodb_user: str
-    mongodb_password: str
-    mongodb_cluster: str
-    mongodb_name: str = "shelfd"
-    tmdb_api_key: str
-    tmdb_base_url: str
-    google_books_base_url: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_DAYS: int
+    MONGODB_USER: str
+    MONGODB_PASSWORD: str
+    MONGODB_CLUSTER: str
+    MONGODB_NAME: str = "shelfd"
+    TMDB_API_KEY: str
+    TMDB_BASE_URL: str
+    GOOGLE_BOOKS_BASE_URL: str
     # Default CORS settings that can be overridden by environment variables
     CORS_ORIGINS: List[str] = ["http://localhost:3000", "https://shelfd-prototype.vercel.app"]
 
     @property
     def mongodb_url(self) -> str:
-        encoded_username = quote_plus(self.mongodb_user)
-        encoded_password = quote_plus(self.mongodb_password)
-        return f"mongodb+srv://{encoded_username}:{encoded_password}@{self.mongodb_cluster}/?retryWrites=true&w=majority"
+        encoded_username = quote_plus(self.MONGODB_USER)
+        encoded_password = quote_plus(self.MONGODB_PASSWORD)
+        return f"mongodb+srv://{encoded_username}:{encoded_password}@{self.MONGODB_CLUSTER}/?retryWrites=true&w=majority"
 
     class Config:
         env_file = ".env"
